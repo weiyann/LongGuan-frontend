ec27d2c5bd6a7f13f29adecf02f1fb681a2050cb
weiyann <jona11105@gmail.com>
2024-02-11 16:42:46 +0800

    引入bootstrap sidebar

---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+
+<template>
+  <div
+    class="d-flex flex-column flex-shrink-0 p-3 text-white bg-success bg-gradient"
+    style="width: 200px; height: 100vh"
+  >
+    <a href="/" class="d-flex align-items-center ms-3 text-white text-decoration-none">
+      <span class="fs-4">LongGuan</span>
+    </a>
+    <hr />
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page"> Home </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white"> Dashboard </a>
+      </li>
+    </ul>
+    <hr />
+    <div class="dropdown">
+      <a
+        href="#"
+        class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+        id="dropdownUser1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <strong>mdo</strong>
+      </a>
+      <ul
+        class="dropdown-menu dropdown-menu-dark text-small shadow"
+        aria-labelledby="dropdownUser1"
+      >
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
